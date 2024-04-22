607869cc6b60af1ee41ad1320c9a0956b55106f0
lessw2020 <lessw@etrillium.com>
2024-04-22 09:55:46 -0700

    more fp8 kernel

--- cutlass_gemm/cutlass_gemm/fp8_practice.hpp
+++ cutlass_gemm/cutlass_gemm/fp8_practice.hpp
@@ -29,3 +29,48 @@ using ElementAux = ElementC;
 using LayoutAux = LayoutC;
 using ElementAmax = float;
 using ElementBias =float;
+
+
+//Core kernel configurations
+using ElementAccumulator = float;                                          // Element type for internal accumulation
+using ElementCompute = float;                                              // Element type for epilogue computation
+using ArchTag = cutlass::arch::Sm90;                                       // Tag indicating the minimum SM that supports the intended feature
+using OperatorClass = cutlass::arch::OpClassTensorOp;                      // Operator class tag
+using TileShape = Shape<_64, _128, _128>;                                  // Threadblock-level tile size
+using ClusterShape = Shape<_1, _2, _1>;                                    // Shape of the threadblocks in a cluster
+using KernelSchedule = cutlass::gemm::KernelTmaWarpSpecialized;
+using EpilogueSchedule = cutlass::epilogue::TmaWarpSpecialized;
+using EpilogueTileType = cutlass::epilogue::collective::EpilogueTileAuto;
+using FusionOperation = cutlass::epilogue::fusion::ScaledLinCombPerRowBiasEltActAmaxAux<
+    LayoutAux, cutlass::epilogue::thread::ReLU, ElementD, ElementCompute, ElementAux, ElementAmax, ElementBias, ElementC>;
+
+using CollectiveEpilogue = typename cutlass::epilogue::collective::CollectiveBuilder<
+    ArchTag, OperatorClass,
+    TileShape, ClusterShape,
+    EpilogueTileType,
+    ElementAccumulator, ElementCompute,
+    ElementC, LayoutC, AlignmentC,
+    ElementD, LayoutD, AlignmentD,
+    EpilogueSchedule,
+    FusionOperation
+    >::CollectiveOp;
+
+using CollectiveMainLoop = typename cutlass::gemm::collective::CollectiveBuilder<
+    ArchTag, OperatorClass,
+    ElementA, LayoutA, AlignmentA,
+    ElementB, LayoutB, AlignmentB,
+    ElementAccumulator,
+    TileShape, ClusterShape,
+    cutlass::gemm::collective::StageCountAutoCarveout<
+    static_cast<int>(sizeof(typename CollectiveEpilogue::SharedStorage))
+    >,
+    KernelSchedulestatic
+    >::CollectiveOp;
+using GemmKernel = cutlass::gemm::kernel::GemmUniversal<
+    Shape<int, int,int, int>, // Indicates ProblemShape
+    CollectiveMainLoop,
+    CollectiveEpilogue
+>;
+
+using Gemm = cutlass::gemm::device::GemmUniversalAdapter<GemmKernel>;
+// ------
